a538f5411a9da343b1e34d7b51aaa46f130b13dd
shi0001-99 <2198876433@qq.com>
2026-09-21 14:44:18 +0800

    add

--- stacks_and_queues/linked_stack.c
+++ stacks_and_queues/linked_stack.c
@@ -1,7 +1,7 @@
 typedef struct{
     Node *base;
     Node *top;
-    int size;
+    int size;//栈当前的大小
 }stack;//实际上是一个保持首尾指针的单链表
 
 void initstack(stack* s){
@@ -10,3 +10,31 @@ void initstack(stack* s){
     s->size=0;
 }
 
+int Push(stack* s,double e){
+    Node *N=(Node*)malloc(sizeof(Node));
+    N->data=e;
+    N->next=s->top;
+    if(!s->base){
+        s->base=s->top=N;
+    }
+    else{
+        s->top=N;
+    }
+    s->size++;
+    return 0;
+}
+
+//十进制转换八进制
+void dec2oct(stack* s,int n){
+    while(n){
+        Push(s,n%8);
+        n/=8;
+    }
+    while(!stackempty(s)){
+        int e;
+        Pop(s,&e);
+        printf("%d",e);
+    }
+}
+
+//括号匹配
